05d2218eec251ba4e748c44ca97d28ad0c11872c
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-22 18:33:57 -0500

    feat: agregar paths

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './page/home-page/home-page.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent
+    component: SideBarComponent
   }
 ];
 
