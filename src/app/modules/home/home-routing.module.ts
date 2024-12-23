61b718528b4184b88e268cdb8e71df3e4c2e4620
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-22 19:07:50 -0500

    feat: agregar estilos al side-bar

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { HomePageComponent } from './page/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: SideBarComponent
+    component: HomePageComponent
   }
 ];
 
