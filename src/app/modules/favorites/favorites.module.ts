c83d91250913f1aa1c460897848b04d48111ce74
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-23 15:53:56 -0500

    feat: agregar contexto al template de tracks

--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
+import { SharedModule } from '@shared/shared.module';
+import { FavoritePageComponent } from './pages/favorite-page/favorite-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FavoritePageComponent
+  ],
   imports: [
     CommonModule,
-    FavoritesRoutingModule
+    FavoritesRoutingModule,
+    SharedModule
   ]
 })
 export class FavoritesModule { }
