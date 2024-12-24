266f3edfc57e9240eddefe1e63e6d319733dd5fb
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-24 15:33:01 -0500

    feat: agregar pipe para ordenar las listas

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -9,9 +9,15 @@ import { TrackModel } from '@core/models/tracks.model';
 })
 export class PlayListBodyComponent implements OnInit {
   tracks: TrackModel[] = [];
+  optionSort: { property: string | null, order: string } = { property: null, order: 'asc' }
 
   ngOnInit(): void {
       const { data }: any = (dataRaw as any).default;
       this.tracks = data;
+  }
+
+  changeSort(property: string): void {
+    const { order } = this.optionSort;
+    this.optionSort = { property, order: (order === 'asc') ? 'desc' : 'asc' }
   }
 }
