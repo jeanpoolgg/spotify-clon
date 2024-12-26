03c64380e0592b52ecb2bf4d09e8e791d6b1f83f
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2024-12-25 19:20:48 -0500

    feat: agregar formgroup de login

---
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent implements OnInit {
+  formLogin: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12)
+      ])
+    })
+  }
+
+
+  sendLogin(): void {
+    const body = this.formLogin.value;
+    console.log(body);
+  }
+}
